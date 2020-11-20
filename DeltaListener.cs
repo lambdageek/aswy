1fe1ebcd889550f5700942f4302cc611c0f724b6
Aleksey Kliger <aleksey@lambdageek.org>
2020-11-19 23:36:59 -0500

    add a websocket

--- DeltaListener.cs
+++ DeltaListener.cs
@@ -23,11 +23,12 @@ public class DeltaListener {
         Task.Run (async () => {
             for (int i = 0; i < 10; ++i) {
                 Console.WriteLine($"doing sockety work {i}");
-                await Task.Delay(50);
+                await Task.Delay(5);
             }
             var bytes = Encoding.UTF8.GetBytes ("Hi there");           
             await socket.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
             await socket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+            doner.SetResult();
         });
     }
 }
--- wwwroot/script.js
+++ wwwroot/script.js
@@ -2,19 +2,45 @@
 (function () {
     'use strict'
 
+    function start_ws (base_url, listener) {
+        let ws = new WebSocket (base_url + "/ws");
+        ws.addEventListener('open', listener.onopen);
+        ws.addEventListener('close', listener.onclose);
+        ws.addEventListener('message', listener.onmessage);
+    }
+
     class MyApp {
         constructor() {
 
             this.bingo = null;
             this.clicky = null;
             this.n = 0;
-
+            this.base_ws = null;
         }
-        onclick(button, evt) {
+
+        onclick(_evt) {
             this.bingo.innerText = `clicked ${++this.n} times`;
             this.clicky.innerText = "click me again";
+            this.clicky.disabled = true;
+            start_ws (this.base_ws, {
+                onopen: (_evt) => { this.bingo.innerText += " and connection opened"},
+                onclose: (_evt) => {
+                    this.clicky.disabled = false;
+                    this.bingo.innerText += " and connection closed"
+                },
+                onmessage: (evt) => { this.bingo.innerText = `got ${++this.n}: ${evt.data}`}
+            })            
         }
-        ready(event) {
+
+        bindProtocolStuff() {
+            let proto = document.location.protocol === 'https:' ? "wss" : "ws";
+            let port = document.location.port ? (':' + document.location.port) : '';
+            let host = document.location.hostname;
+            this.base_ws = proto + "://" + host + port;
+        }
+
+        ready(_event) {
+            this.   bindProtocolStuff ();
             this.bingo = document.getElementById("bingo");
             this.bingo.innerText = "ready to go";
             this.clicky = document.getElementById("clicky");
